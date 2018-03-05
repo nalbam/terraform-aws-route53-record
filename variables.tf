d16bac26689e2ad2435712d5676c5f4b509eb071
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-05 13:29:29 +0900

    description

--- variables.tf
+++ variables.tf
@@ -4,20 +4,25 @@ variable region {
 
 variable zone_id {
   //default = ""
+  description = "The ID of the hosted zone to contain this record."
 }
 
 variable name {
   //default = ""
+  description = "The name of the record."
 }
 
 variable type {
   //default = ""
+  description = "The record type. Valid values are A, AAAA, CAA, CNAME, MX, NAPTR, NS, PTR, SOA, SPF, SRV and TXT."
 }
 
 variable ttl {
   default = "300"
+  description = "The TTL of the record."
 }
 
 variable record {
   //default = ""
+  description = "A string list of records."
 }
